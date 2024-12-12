915b2f7fc9004c9ea532474e15dfa84da40202ba
grygoriev <vovagrigoriev@gmail.com>
2024-12-12 16:02:04 +0200

    tack code

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-import './index.css';
+import './index.css'
+
+const cookieConsent = document.querySelector(".cookie-consent");
+const acceptCookieButton = document.querySelector(".cookie-consent__button");
+
+const COOKIE_ACCEPTED_KEY = "cookieAccepted";
+
+function isCookieAccepted() {
+    return localStorage.getItem(COOKIE_ACCEPTED_KEY) === "true";
+}
+
+function hideCookieConsent() {
+    cookieConsent.style.display = "none";
+}
+
+function acceptCookies() {
+    localStorage.setItem(COOKIE_ACCEPTED_KEY, "true");
+    hideCookieConsent();
+}
+
+function initCookieConsent() {
+    if (isCookieAccepted()) {
+        hideCookieConsent();
+    }
+}
+
+acceptCookieButton.addEventListener("click", acceptCookies);
+
+initCookieConsent();
